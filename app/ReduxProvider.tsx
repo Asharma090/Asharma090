7603b226e604fd0a64ca2f158ec68388aeb652e8
Abhishek <abhisheksharma.oct090@gmail.com>
2023-10-09 16:32:40 -0500

    fixed type error

--- app/ReduxProvider.tsx
+++ app/ReduxProvider.tsx
@@ -1,7 +1,12 @@
 "use client";
 import { Provider } from "react-redux"
 import { store } from "../store/store";
+import { ReactNode } from "react";
 
-export default function ReduxProvider({ children }) {
+interface Props {
+  children?: ReactNode
+}
+
+export default function ReduxProvider({ children }: Props) {
   return <Provider store={store}>{children}</Provider>;
 };
